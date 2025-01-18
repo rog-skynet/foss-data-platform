d1605a57a68531e84566578510fd212b6f512e85
Gunnar K. Halvorsen <gunnar.halvorsen@bouvet.no>
2025-01-18 12:44:09 +0100

    hotfix

--- master.sh
+++ master.sh
@@ -1,12 +1,10 @@
-### Master Setup Script: master.sh
 #!/bin/bash
 
-# Run the setup scripts as root for initial configuration
-bash setup/setup_user.sh
+# Run the user-specific setup script as root (datauser doesn't exist yet)
+bash deploy/user.sh
 
 # Switch to datauser for subsequent setup steps
 sudo -u datauser bash -c "bash deploy/minio.sh"
-sudo -u datauser bash -c "bash deploy/user.sh"
 sudo -u datauser bash -c "bash deploy/trino.sh"
 sudo -u datauser bash -c "bash deploy/dagster.sh"
 sudo -u datauser bash -c "bash deploy/dbt.sh"
